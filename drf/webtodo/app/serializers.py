a88f9e9e032767ada6ce3293da8bc0617b713a43
alexeyb7 <97844430+alexeyb7@users.noreply.github.com>
2023-01-10 04:27:06 +0300

    Delete drf directory

--- drf/webtodo/app/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.serializers import HyperlinkedModelSerializer
-from .models import Users
-
-
-class UsersModelSerializer(HyperlinkedModelSerializer):
-    class Meta:
-        model = Users
-        fields = 'email', 'firstname', 'lastname', 'username'
--- drf/webtodo/app/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.viewsets import ModelViewSet
-from .models import Users
-from .serializers import UsersModelSerializer
-
-
-class UsersModelViewSet(ModelViewSet):
-    queryset = Users.objects.all()
-    serializer_class = UsersModelSerializer
